584d26162527edf623a59ce3bc567a8cf6b1060f
Yashwanth B M <yashwanthbm36@gmail.com>
2023-12-05 10:24:32 +0530

    Created environment variables

--- Blog_Website/src/App.jsx
+++ Blog_Website/src/App.jsx
@@ -1,33 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import config from './config/config'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+    console.log(config.appwriteUrl)
+    console.log(config.appwriteProjectId)
+    console.log(config.appwriteDatabaseId)
+    console.log(config.appwriteCollectionId)
+    console.log(config.appwriteBucketId)
+   
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Blog with AppWrite!</h1>
+    {
+
+    }
     </>
   )
 }
