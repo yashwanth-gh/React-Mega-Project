f71910ff0e87645a5c5286525233c13cfd68370f
Yashwanth B M <yashwanthbm36@gmail.com>
2023-12-07 22:42:14 +0530

    added post card feature

---
+++ Blog_Website/src/components/Select.jsx
@@ -0,0 +1,27 @@
+import React, { forwardRef, useId } from 'react'
+
+const Select = forwardRef(function Select({
+    options,
+    label,
+    className = '',
+    ...props
+},ref){
+    const id =  useId();
+    return (
+        <div className='w-full'>
+            {label && <label htmlFor={id} className=''></label>}
+            <select id={id}
+            ref={ref}
+            className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+            {...props}>
+                {options?.map(option=>(
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+        </div>
+    )
+})
+
+export default Select
--- Blog_Website/src/components/index.js
+++ Blog_Website/src/components/index.js
@@ -3,6 +3,11 @@ import Header from './Header/Header';
 import LogoutBtn from './Header/LogoutBtn';
 import Logo from './Logo';
 import Container from './container/Container';
+import Button from './Button'
+import Input from './Input'
+import Login from './Login'
+import PostCard from './PostCard'
+import Select from './Select'
 
 export {
     Footer,
@@ -10,4 +15,9 @@ export {
     Logo,
     LogoutBtn,
     Container,
+    Button,
+    Input,
+    Login,
+    PostCard,
+    Select,
 };
