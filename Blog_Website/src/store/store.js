57014aca50aadc775d2609581fbc5cc1f84b0126
Yashwanth B M <yashwanthbm36@gmail.com>
2023-12-12 22:53:59 +0530

    updated store

--- Blog_Website/src/store/store.js
+++ Blog_Website/src/store/store.js
@@ -2,6 +2,8 @@ import {configureStore} from '@reduxjs/toolkit'
 import authReducer from './authSlice'
 const store = configureStore({
     reducer:{
+        // here when we create object and then we make reducers of variours slices as KEYS
+        // you have to access the reducer in useSelector as state.auth.variable_name
         auth:authReducer,
 
     }
